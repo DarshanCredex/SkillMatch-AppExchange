1cb23b8912d545cb71116d30f4ed5a9c5f20d909
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-12-21 15:34:28 +0530

    added upload button functionality

--- force-app/main/default/lwc/uploadResume/uploadResume.js
+++ force-app/main/default/lwc/uploadResume/uploadResume.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class UploadResume extends LightningElement {}
+export default class UploadResume extends LightningElement {
+
+    uploadedFile;
+    filename;
+    handleUpload(event){
+        let file = event.target.files[0];
+        this.filename=file.name;
+        console.log('this.filename-------->',this.filename);
+        this.uploadedFile=file;
+    }
+}
