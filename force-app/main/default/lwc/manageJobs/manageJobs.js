47da4aabba65f0694ddd0a8625610908d955b141
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-01-04 14:22:31 +0530

    built the manage jobs component

--- force-app/main/default/lwc/manageJobs/manageJobs.js
+++ force-app/main/default/lwc/manageJobs/manageJobs.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
+import no_jobs from "@salesforce/resourceUrl/no_jobs";
 
-export default class ManageJobs extends LightningElement {}
+export default class ManageJobs extends LightningElement {
+    noJobs = no_jobs;
+}
