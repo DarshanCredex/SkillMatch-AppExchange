551397d494e9b6a688adb382a1f0356edd2506ae
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-01-09 13:31:54 +0530

    added pagereference to navigate to add jobs page

--- force-app/main/default/lwc/manageJobs/manageJobs.js
+++ force-app/main/default/lwc/manageJobs/manageJobs.js
@@ -1,6 +1,21 @@
 import { LightningElement } from "lwc";
 import no_jobs from "@salesforce/resourceUrl/no_jobs";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class ManageJobs extends LightningElement {
+export default class ManageJobs extends NavigationMixin(LightningElement) {
+  
   noJobs = no_jobs;
+
+  handlePageReference() {
+    const pageReference = {
+      type: "standard__webPage",
+      attributes: {
+        url: "/add-jobs"
+      }
+    };
+    this[NavigationMixin.Navigate](pageReference);
+  }
+
+
+
 }
