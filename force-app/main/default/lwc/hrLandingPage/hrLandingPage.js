3be09f46f1056d932b73c170e348d01b24765ae2
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-01-11 13:12:15 +0530

    added drafts and tasks tables to HR landing page

--- force-app/main/default/lwc/hrLandingPage/hrLandingPage.js
+++ force-app/main/default/lwc/hrLandingPage/hrLandingPage.js
@@ -5,18 +5,50 @@ import Id from "@salesforce/user/Id";
 import No_tasks from "@salesforce/resourceUrl/No_tasks";
 import SkillMatch_Logo_White from "@salesforce/resourceUrl/skillMatch_logo";
 import no_drafts from "@salesforce/resourceUrl/no_drafts";
+import getTaskList from "@salesforce/apex/TaskController.getTaskList";
+import getDraftJobList from "@salesforce/apex/jobObjectController.getDraftJobList";
 
 export default class HrLandingPage extends LightningElement {
   @track currentUserName;
+  @track taskList = [];
+  @track draftJobList = [];
   image = No_tasks;
   no_drafts = no_drafts;
   Skillmatch_logo = SkillMatch_Logo_White;
+  showPendingTasks = false;
+  showDrafts = false;
+
   @wire(getRecord, { recordId: Id, fields: [UserNameFIELD] })
   currentUserInfo({ error, data }) {
     if (data) {
       this.currentUserName = data.fields.Name.value;
     } else if (error) {
       this.error = error;
+    }
+  }
+
+  //
+  connectedCallback() {
+    getTaskList()
+      .then((data) => {
+        this.taskList = data;
+        console.log("this.taskList-------->", this.taskList);
+        this.showPendingTasks = true;
+      })
+      .catch((error) => {
+        console.log("error------>", error);
+      });
+  }
+
+  @wire(getDraftJobList)
+  wiredgGetDraftJobList({ error, data }) {
+    if (data) {
+      console.log("data----->", data);
+      this.draftJobList = data;
+      console.log("this.draftJobList------>", this.draftJobList);
+      this.showDrafts = true;
+    } else if (error) {
+      console.log("error------->", error);
     }
   }
 }
