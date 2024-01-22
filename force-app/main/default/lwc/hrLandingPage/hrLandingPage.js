288fcc47c5348ec1b367af5371124a2918d8d8bd
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-01-22 13:29:14 +0530

    homepage redesigned structure done

--- force-app/main/default/lwc/hrLandingPage/hrLandingPage.js
+++ force-app/main/default/lwc/hrLandingPage/hrLandingPage.js
@@ -1,54 +1,23 @@
 import { LightningElement, track, wire } from "lwc";
-import { getRecord } from "lightning/uiRecordApi";
 import UserNameFIELD from "@salesforce/schema/User.Name";
 import Id from "@salesforce/user/Id";
-import No_tasks from "@salesforce/resourceUrl/No_tasks";
-import SkillMatch_Logo_White from "@salesforce/resourceUrl/skillMatch_logo";
-import no_drafts from "@salesforce/resourceUrl/no_drafts";
-import getTaskList from "@salesforce/apex/TaskController.getTaskList";
-import getDraftJobList from "@salesforce/apex/jobObjectController.getDraftJobList";
+import { getRecord } from "lightning/uiRecordApi";
 
 export default class HrLandingPage extends LightningElement {
   @track currentUserName;
-  @track taskList = [];
-  @track draftJobList = [];
-  image = No_tasks;
-  no_drafts = no_drafts;
-  Skillmatch_logo = SkillMatch_Logo_White;
-  showPendingTasks = false;
-  showDrafts = false;
+  currentDate = new Date().toDateString();
 
-  @wire(getRecord, { recordId: Id, fields: [UserNameFIELD] })
+  @wire(getRecord, {
+    recordId: Id,
+    fields: [UserNameFIELD]
+  })
   currentUserInfo({ error, data }) {
     if (data) {
       this.currentUserName = data.fields.Name.value;
+      console.log("this.currentUserName-------->", this.currentUserName);
     } else if (error) {
       this.error = error;
-    }
-  }
-
-  //
-  connectedCallback() {
-    getTaskList()
-      .then((data) => {
-        this.taskList = data;
-        console.log("this.taskList-------->", this.taskList);
-        this.showPendingTasks = true;
-      })
-      .catch((error) => {
-        console.log("error------>", error);
-      });
-  }
-
-  @wire(getDraftJobList)
-  wiredgGetDraftJobList({ error, data }) {
-    if (data) {
-      console.log("data----->", data);
-      this.draftJobList = data;
-      console.log("this.draftJobList------>", this.draftJobList);
-      this.showDrafts = true;
-    } else if (error) {
-      console.log("error------->", error);
+      console.log("this.error", this.error);
     }
   }
 }
