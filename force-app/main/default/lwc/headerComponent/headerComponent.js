342611cdd8f2d70fec934045b43fc9af9c39ab1a
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-02-29 15:48:20 +0530

    added hero component and  navigation

--- force-app/main/default/lwc/headerComponent/headerComponent.js
+++ force-app/main/default/lwc/headerComponent/headerComponent.js
@@ -1,7 +1,49 @@
 import { LightningElement } from "lwc";
 import skillMatch_logo from "@salesforce/resourceUrl/skillMatch_logo";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class HeaderComponent extends LightningElement {
-    skillMatch_logo = skillMatch_logo;
-    
+export default class HeaderComponent extends NavigationMixin(LightningElement) {
+  skillMatch_logo = skillMatch_logo;
+
+  myjobs = false;
+  jobs = false;
+  home = true;
+
+  navigateToJobs() {
+    const pageReference = {
+      type: "standard__webPage",
+      attributes: {
+        url: "/job-list"
+      }
+    };
+    this[NavigationMixin.Navigate](pageReference);
+     this.home = false;
+     this.myjobs = false;
+     this.jobs = true;
+  }
+
+  navigateToHome() {
+    const pageReference = {
+      type: "standard__webPage",
+      attributes: {
+        url: "/"
+      }
+    };
+    this[NavigationMixin.Navigate](pageReference);
+    this.home = true;
+    this.myjobs = false;
+    this.jobs = false;
+  }
+  navigateToMyJobs() {
+    const pageReference = {
+      type: "standard__webPage",
+      attributes: {
+        url: "/my-jobs"
+      }
+    };
+    this[NavigationMixin.Navigate](pageReference);
+     this.home =false;
+     this.myjobs = true;
+     this.jobs = false;
+  }
 }
---
+++ force-app/main/default/lwc/heroComponent/heroComponent.js
@@ -0,0 +1,9 @@
+import { LightningElement } from "lwc";
+import heroImage from "@salesforce/resourceUrl/heroImage";
+
+export default class HeroComponent extends LightningElement {
+    
+    heroImage = heroImage;
+    
+
+}
