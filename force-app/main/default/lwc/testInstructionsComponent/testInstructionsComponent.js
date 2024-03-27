85ee406bbc56bc6c536c91ed68ab49c1d3a00023
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-03-27 12:23:53 +0530

    added button and time for assesment

--- force-app/main/default/lwc/testInstructionsComponent/testInstructionsComponent.js
+++ force-app/main/default/lwc/testInstructionsComponent/testInstructionsComponent.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import getTestTimings from "@salesforce/apex/testingEnvironmentController.getTestTimings";
 
-export default class TestInstructionsComponent extends LightningElement {}
+export default class TestInstructionsComponent extends LightningElement {
+  testTiming;
+  jobId;
+
+  connectedCallback() {
+    this.jobId = sessionStorage.getItem("jobId");
+    console.log("this.jobId---->", this.jobId);
+  }
+
+  @wire(getTestTimings, { jobId: "$jobId" })
+  wiredGetTestTimings({ data, error }) {
+    if (data) {
+      this.testTiming = data;
+      console.log("this.testTiming->", this.testTiming);
+    } else {
+      const err = error;
+      console.error(err);
+    }
+  }
+}
