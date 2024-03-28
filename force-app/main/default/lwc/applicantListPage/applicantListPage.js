44a13f520bdec95f14ca2bf4af6e700305642552
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-03-28 19:16:05 +0530

    Added mulitple applicant select functionality

--- force-app/main/default/lwc/applicantListPage/applicantListPage.js
+++ force-app/main/default/lwc/applicantListPage/applicantListPage.js
@@ -1,8 +1,11 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, wire } from "lwc";
 import fetchCandidateNames from "@salesforce/apex/JobApplicantController.fetchCandidateNames";
 import fetchJobDetails from "@salesforce/apex/JobApplicantController.fetchJobDetails";
 import emptyBox from "@salesforce/resourceUrl/empty_box";
 import { NavigationMixin } from "lightning/navigation";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import changeStatus from "@salesforce/apex/JobApplicantController.changeStatus";
+import { refreshApex } from "@salesforce/apex";
 
 export default class ApplicantListPage extends NavigationMixin(
   LightningElement
@@ -10,6 +13,9 @@ export default class ApplicantListPage extends NavigationMixin(
   candidateDetails = [];
   @track filteredCandidateDetails = [];
   jobDetails = [];
+  allIdList = [];
+  selectedIdList = [];
+  wiredResult = [];
 
   candidateId;
   jobId;
@@ -19,21 +25,29 @@ export default class ApplicantListPage extends NavigationMixin(
 
   candiateListIsEmpty = false;
   filteredListIsEmpty = false;
+  @track checkboxSelected = false;
+  selectAllChecked = false;
+  disableButtons = true;
+  disableRejectedCheckbox = false;
 
   connectedCallback() {
     if (sessionStorage.getItem("uniquejobId")) {
       this.jobId = sessionStorage.getItem("uniquejobId");
     }
-    this.fetchCandidateNames();
     this.fetchJobDetails();
   }
 
-  fetchCandidateNames() {
-    fetchCandidateNames({ jobId: this.jobId }).then((data) => {
-      this.candidateDetails = data; // source of truth
-      this.filteredCandidateDetails = this.candidateDetails;
+  @wire(fetchCandidateNames, { jobId: "$jobId" })
+  wiredCandidates(result) {
+    this.wiredResult = result;
+    if (result.data) {
+      this.candidateDetails = result.data;
+      this.filteredCandidateDetails = result.data;
       this.candiateListIsEmpty = this.filteredCandidateDetails.length === 0;
-    });
+      this.allIdList = this.filteredCandidateDetails.map(
+        (candidate) => candidate.Id
+      );
+    }
   }
 
   fetchJobDetails() {
@@ -74,5 +88,77 @@ export default class ApplicantListPage extends NavigationMixin(
       return item.matchPercentage >= sliderValue;
     });
     this.filteredListIsEmpty = this.filteredCandidateDetails.length === 0;
+  }
+
+  handleCheckBoxSelect(event) {
+    const candidateId = event.target.dataset.checkboxid;
+    var isChecked = false;
+    isChecked = event.target.checked;
+
+    if (isChecked) {
+      this.selectedIdList.push(candidateId);
+      if (this.allIdList.length === this.selectedIdList.length) {
+        this.selectAllChecked = true;
+      }
+      this.disableButtons = false;
+    } else {
+      const index = this.selectedIdList.indexOf(candidateId);
+      this.selectedIdList.splice(index, 1);
+      this.selectAllChecked = false;
+      if (this.selectedIdList.length === 0) {
+        this.disableButtons = true;
+      }
+    }
+  }
+
+  handleSelectAll(event) {
+    this.selectAllChecked = event.target.checked;
+    console.log("select all checked value---->", this.selectAllChecked);
+    if (this.selectAllChecked) {
+      this.checkboxSelected = true;
+      this.selectedIdList = this.filteredCandidateDetails.map(
+        (candidate) => candidate.Id
+      );
+      this.disableButtons = false;
+    } else {
+      this.checkboxSelected = false;
+      this.selectedIdList = [];
+      this.disableButtons = true;
+    }
+  }
+  handleShortlistReject(event) {
+    const valueOfButton = event.target.value;
+    changeStatus({
+      value: valueOfButton,
+      applicantId: this.selectedIdList
+    })
+      .then(() => {
+        if (valueOfButton === "Accepted") {
+          this.showToast(
+            "Shorlisted",
+            "Your selected applicants have been shortlisted",
+            "success"
+          );
+        } else if (valueOfButton === "Rejected") {
+          this.showToast(
+            "Rejected",
+            "Your selected applicants have been rejected",
+            "error"
+          );
+        }
+        refreshApex(this.wiredResult);
+      })
+      .catch(() => {
+        this.showToast("Error", "Some Error occurred", "error");
+      });
+  }
+  showToast(title, message, variant) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant
+      })
+    );
   }
 }
