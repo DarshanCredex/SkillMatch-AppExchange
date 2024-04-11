fac6d5fc7aa1349284308471ebbf8cca792cf216
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-04-11 13:32:58 +0530

    fixed the start test display issues

--- force-app/main/default/lwc/testInstructionsComponent/testInstructionsComponent.js
+++ force-app/main/default/lwc/testInstructionsComponent/testInstructionsComponent.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-unused-vars */
-import { LightningElement, wire } from "lwc";
+import { LightningElement } from "lwc";
 import getTestTimings from "@salesforce/apex/testingEnvironmentController.getTestTimings";
 import { NavigationMixin } from "lightning/navigation";
-import checkIfAlreadyAttempted from "@salesforce/apex/testingEnvironmentController.checkIfAlreadyAttempted";
+import getAssesmentStatus from "@salesforce/apex/testingEnvironmentController.getAssesmentStatus";
 import Id from "@salesforce/user/Id";
+import changeAssesmentStatus from "@salesforce/apex/testingEnvironmentController.changeAssesmentStatus";
 
 export default class TestInstructionsComponent extends NavigationMixin(
   LightningElement
@@ -12,6 +12,7 @@ export default class TestInstructionsComponent extends NavigationMixin(
   jobId;
   disableButton = false;
   userId = Id;
+  AssesmentStatusOfApplicant;
 
   connectedCallback() {
     this.jobId = sessionStorage.getItem("jobId");
@@ -20,15 +21,29 @@ export default class TestInstructionsComponent extends NavigationMixin(
         this.testTiming = result;
       }
     });
-    checkIfAlreadyAttempted({
-      userid: this.userId,
-      jobid: this.jobId
-    }).then((result) => {
-      if (result === true) this.disableButton = true;
-    });
+    getAssesmentStatus({ userid: this.userId, jobid: this.jobId })
+      .then((result) => {
+        const status = result;
+        console.log("status------>", status);
+        if (status === "Pending") {
+          this.disableButton = false;
+        } else {
+          this.disableButton = true;
+        }
+      })
+      .catch((error) => {
+        console.error("error------>", error);
+      });
   }
-
   handleStartTest() {
+
+    changeAssesmentStatus({ userid: this.userId, jobid: this.jobId })
+      .then(() => {
+        console.log("status changed");
+      })
+      .catch((error) => {
+        console.error("error-------->", error);
+      });
     sessionStorage.setItem("jobId", this.jobId);
     const currentURl = window.location.href;
     const newUrl = currentURl.replace(
